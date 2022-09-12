4bb0150c356d91d375d94715c924c40756b0fc04
samarthidea1 <samarthshrivastava46@gmail.com>
2022-09-12 10:31:22 +0000

    message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Heyyy !!!');
+  res.send('Heyyyaa !!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
