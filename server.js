c0970efb22841878ed229c268f76764d1ee833b9
samarthidea1 <samarthshrivastava46@gmail.com>
2022-09-14 10:43:28 +0000

    jenkinsfilefk

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Heyy yaead==ds wyd?? !!!');
+  res.send('Heyy yaeadds wyd?? !!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
